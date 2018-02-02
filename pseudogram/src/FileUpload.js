78bfd499f4e56769bc1c51332911cb3038d2ce5f
Lucas Romero Di Benedetto <lro@3818.com.ar>
2018-02-02 14:28:16 -0300

    cambios menores

--- pseudogram/src/FileUpload.js
+++ pseudogram/src/FileUpload.js
@@ -1,6 +1,20 @@
 import React, {Component} from 'react';
 // import firebase from 'firebase';
 
+
+var buttonstyle = {
+  padding: 20,
+  borderRadius: 5,
+  border: 'none',
+  cursor: 'pointer',
+  // boxShadow: '0 3px 15px lightgray',
+  border: '1px solid lightgray',
+  marginTop: 20,
+  backgroundColor: 'white',
+  color: 'gray',
+  fontSize: 16
+}
+
 class FileUpload extends Component {
   constructor() {
     super();
@@ -14,7 +28,8 @@ class FileUpload extends Component {
       <div>
         <progress value={this.state.uploadValue} max="100"></progress>
         <br />
-        <input type="file" onChange={this.props.onUpload} />
+        <input type="file" style={buttonstyle} onChange={this.props.onUpload} />
+        <hr />
       </div>
     )
   }
